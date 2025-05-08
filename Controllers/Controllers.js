c75cb2a6bf81b8a6593dbe767b9bdba1e544f909
Raff_12 <raffelecoppola381@gmail.com>
2025-05-08 23:20:24 +0200

    feat: Modifica della chiamata API

--- Controllers/Controllers.js
+++ Controllers/Controllers.js
@@ -26,7 +26,19 @@ function Delete_Post(req, res) {
     })
 }
 
+function Search_Post(req, res) {
+    let id = String(req.params.id).slice(1, 2);
+    console.log(id);
+    const keyword = "SELECT * FROM posts WHERE id = ?";
+     connection.query(keyword, [id], (error, results) => {
+        if (error) return res.status(500).json({msg: "Errore nel Database", code: 500});
+        if (results.length === 0) return res.status(404).json({msg: "La Ricerca non è andata a Buon Fine.", code: 404});
+        return res.status(200).json({msg: "Il Post è stato trovato", code: 200, result: results});
+    })
+}
+
 module.exports = {
     Show_list,
-    Delete_Post
+    Delete_Post,
+    Search_Post
 }
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -6,12 +6,15 @@
 
 const express = require("express");
 const routes = express.Router();
-const { Show_list, Delete_Post } = require("../Controllers/Controllers.js");
+const { Show_list, Delete_Post, Search_Post } = require("../Controllers/Controllers.js");
 
 // Chiamata Get
 routes.get("/posts", Show_list);
 
 // Chiamata Delete
 routes.delete("/posts/:id", Delete_Post);
+
+// Chiamata Post
+routes.post("/posts/:id", Search_Post);
 
 module.exports = routes;
